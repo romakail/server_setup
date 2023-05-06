d79b839151a32ce1f749ef1a0beaaf33f8a2fb12
students <rvkail@beleriand.velkerr.ru>
2023-05-06 09:23:51 +0000

    zshrc and tmux.conf

---
+++ run.sh
@@ -0,0 +1,14 @@
+### ZSH
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ./zshrc ${HOME}/.zshrc
+
+chsh -s $(which zsh)
+
+### TMUX
+
+sudo apt-get install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mv tmux ~/.tmux.conf
+
+# Don't forget to press ctrl+a and shift+i to install the plugins 
